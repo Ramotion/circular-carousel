159a4f924257c7e654cfcce4c446cf1eb108cfce
Piotr Suwara <psuwara@realityworks.com.au>
2019-01-02 15:55:12 +0300

    - Added a simple tableview will cells to be able to list the Carousel View in a TableView.

---
+++ Demo/RACarousel Demo/RACarousel Demo/CarouselTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselTableViewCell.swift
+//  RACarousel Demo
+//
+//  Created by Piotr Suwara on 2/1/19.
+//  Copyright © 2019 Piotr Suwara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CarouselTableViewCell : UITableViewCell {
+    @IBOutlet weak var carousel : RACarousel!
+    @IBOutlet weak var title : UILabel!
+}
--- Demo/RACarousel Demo/RACarousel Demo/ViewController.swift
+++ Demo/RACarousel Demo/RACarousel Demo/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    static let CarouselRow = 3
+    static let NumberOfRows = 10
+    static let CarouselTableViewCellIdentifier = "CarouselTableViewCellIdentifier"
+    static let UITableViewCellIdentifier = "UITableViewCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register custom cell for carousel
+        tableView.register(UINib(nibName: "CarouselTableViewCell", bundle: nil), forCellReuseIdentifier: ViewController.CarouselTableViewCellIdentifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ViewController.UITableViewCellIdentifier)
     }
 
-
+    // MARK: -
+    // MARK: UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        
+        switch row {
+        case ViewController.CarouselRow:
+            let cell: CarouselTableViewCell = tableView.dequeueReusableCell(withIdentifier: ViewController.CarouselTableViewCellIdentifier) as! CarouselTableViewCell
+            
+            cell.title.text = "CAROUSEL CELL"
+            return cell
+            
+        default:
+            let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: ViewController.UITableViewCellIdentifier)!
+            
+            cell.textLabel?.text = "CELL : \(row)"
+            return cell
+            
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ViewController.NumberOfRows
+    }
 }
 
